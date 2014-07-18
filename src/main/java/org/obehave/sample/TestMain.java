7ac062c2465c0817645562688e0d68311eb3d9f1
Markus Möslinger <markus.moeslinger@gmail.com>
2014-07-18 22:02:04 +0200

    basic stuff for testing of ci

---
+++ src/main/java/org/obehave/sample/TestMain.java
@@ -0,0 +1,10 @@
+package org.obehave.sample;
+
+/**
+ * @author outofrange
+ */
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("A very first test");
+    }
+}
