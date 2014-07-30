7078e8a0ee9b95c8af5fdacbce397e22c6b14c18
Markus Möslinger <markus.moeslinger@gmail.com>
2014-07-30 19:53:25 +0200

    renamed file

--- src/main/java/org/obehave/sample/TestMain.java
+++ src/main/java/org/obehave/sample/TestMain.java
@@ -4,7 +4,7 @@ package org.obehave.sample;
  * @author outofrange
  */
 public class TestMain {
-    public static void main(String[] args) {
+    public static void bla(String[] args) {
         System.out.println("A very first commit - ci works \\o/");
     }
 }
