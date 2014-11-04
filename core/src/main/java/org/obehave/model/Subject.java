c8231de742ffb138803d4cd43708f35874d9e2f9
Markus Möslinger <markus.moeslinger@gmail.com>
2014-11-05 00:30:05 +0100

    #44 Als Developer möchte ich eine Grundstruktur für die Erstellung der ersten Repositories...
    - added dummy name for subject
    - (re)annotated fields in BaseEntity
    - added DateUtil
    - added sample DaoTest (mainly to learn how to use OrmLite)
    - oh, made a custom LocalDateTimeType \o/

--- core/src/main/java/org/obehave/model/Subject.java
+++ core/src/main/java/org/obehave/model/Subject.java
@@ -1,7 +1,21 @@
 package org.obehave.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
 /**
  * This class describes the observed subjects of a study
  */
+@Entity
 public class Subject extends BaseEntity {
+    @Column
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
