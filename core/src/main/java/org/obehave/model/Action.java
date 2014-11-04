116e69c95220799f423f0c0cf9416e7a653604a1
Markus Möslinger <markus.moeslinger@gmail.com>
2014-11-04 19:47:58 +0100

    obehave-44 Als Developer möchte ich eine Grundstruktur für die Erstellung der ersten Repositories...
    - added four basic entities

---
+++ core/src/main/java/org/obehave/model/Action.java
@@ -0,0 +1,13 @@
+package org.obehave.model;
+
+/**
+ * This class describes actions a subject is able to perform.
+ * There are several types of actions:
+ *
+ * Point and state actions
+ * Single actions or interactions between different subjects
+ *
+ * Actions can be modified in some way.
+ */
+public class Action extends BaseEntity {
+}
