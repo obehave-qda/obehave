b06a31a8b72a23d7b0930bb21b42ed0c47605074
Markus Möslinger <markus.moeslinger@gmail.com>
2014-11-05 15:35:24 +0100

    #44 Als Developer möchte ich eine Grundstruktur für die Erstellung der ersten Repositories...
    - added a meaningful test for subject
    - muted logs for ormlite

--- core/src/test/java/org/obehave/persistance/SubjectDaoTest.java
+++ core/src/test/java/org/obehave/persistance/SubjectDaoTest.java
@@ -8,14 +8,23 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.obehave.model.Subject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
 
 /**
  * Created by Markus.Moeslinger on 04.11.2014.
  */
 public class SubjectDaoTest {
+    private static final Logger log = LoggerFactory.getLogger(SubjectDaoTest.class);
+
     private JdbcConnectionSource cs;
     private Dao<Subject, Long> dao;
-    private Subject s1;
+    private Subject testSubject;
 
     @Before
     public void prepare() throws Exception {
@@ -25,7 +34,8 @@ public class SubjectDaoTest {
         TableUtils.createTable(cs, Subject.class);
         dao = DaoFactory.createDao(cs, Subject.class);
 
-        s1 = new Subject();
+        testSubject = new Subject();
+        testSubject.setName("Test");
     }
 
     @After
@@ -34,13 +44,24 @@ public class SubjectDaoTest {
     }
 
     @Test
-    public void firstTest() throws Exception {
-        Subject s = new Subject();
-        s.setName("Hubert");
-        System.out.println("Create: " + dao.create(s));
+    public void testLocalDateTime() throws Exception {
+        LocalDateTime creation = LocalDate.now().atStartOfDay();
+        LocalDateTime modified = creation.plusHours(1);
 
-        for (Subject ss : dao.queryForAll()) {
-            System.out.println("Found: " + ss.getName());
-        }
+        log.info("Created timestamps.\ncreationTS: {}\nmodifiedTS: {}", creation, modified);
+
+        testSubject.setCreationTS(creation);
+        testSubject.setModifiedTS(modified);
+
+        log.info("Saving subject {}", testSubject);
+        int id = dao.create(testSubject);
+        log.info("Saved subject has id {}", id);
+
+        Subject loadedSubject = dao.queryForId(Long.valueOf(id));
+        log.info("Loaded subject: {}", loadedSubject);
+
+        log.info("Checking equalness of timestamps");
+        assertThat(loadedSubject.getCreationTS(), is(equalTo(creation)));
+        assertThat(loadedSubject.getModifiedTS(), is(equalTo(modified)));
     }
 }
