88cd8a42d2169ede8b95e34ea308d88084d4e79e
Markus Möslinger <markus.moeslinger@gmail.com>
2014-11-05 15:20:46 +0100

    #44 Als Developer möchte ich eine Grundstruktur für die Erstellung der ersten Repositories...
    - restructured test

--- core/src/test/java/org/obehave/persistance/SubjectDaoTest.java
+++ core/src/test/java/org/obehave/persistance/SubjectDaoTest.java
@@ -4,6 +4,8 @@ import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.spring.DaoFactory;
 import com.j256.ormlite.table.TableUtils;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.obehave.model.Subject;
 
@@ -11,15 +13,28 @@ import org.obehave.model.Subject;
  * Created by Markus.Moeslinger on 04.11.2014.
  */
 public class SubjectDaoTest {
-    @Test
-    public void firstTest() throws Exception {
+    private JdbcConnectionSource cs;
+    private Dao<Subject, Long> dao;
+    private Subject s1;
+
+    @Before
+    public void prepare() throws Exception {
         Class.forName("org.h2.Driver");
-        JdbcConnectionSource cs = new JdbcConnectionSource("jdbc:h2:mem:testdb;");
+        cs = new JdbcConnectionSource("jdbc:h2:mem:testdb;");
 
         TableUtils.createTable(cs, Subject.class);
+        dao = DaoFactory.createDao(cs, Subject.class);
 
-        Dao<Subject, Long> dao = DaoFactory.createDao(cs, Subject.class);
+        s1 = new Subject();
+    }
 
+    @After
+    public void cleanUp() throws Exception {
+        cs.close();
+    }
+
+    @Test
+    public void firstTest() throws Exception {
         Subject s = new Subject();
         s.setName("Hubert");
         System.out.println("Create: " + dao.create(s));
@@ -27,9 +42,5 @@ public class SubjectDaoTest {
         for (Subject ss : dao.queryForAll()) {
             System.out.println("Found: " + ss.getName());
         }
-
-
-
-
     }
 }
