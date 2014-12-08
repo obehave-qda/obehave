93f7e81bb0205a2bef433ecddcbfad6d593c5b84
Markus Möslinger <markus.moeslinger@gmail.com>
2014-12-08 23:24:11 +0100

    - improved timeline

--- desktop/src/main/java/org/obehave/view/controller/components/coding/TimelinePane.java
+++ desktop/src/main/java/org/obehave/view/controller/components/coding/TimelinePane.java
@@ -5,12 +5,19 @@ import javafx.beans.property.SimpleDoubleProperty;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.FontSmoothingType;
 import javafx.scene.text.Text;
+import org.joda.time.Duration;
+import org.joda.time.format.PeriodFormatter;
+import org.joda.time.format.PeriodFormatterBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 // == Weekpane
 public class TimelinePane extends Pane {
     private static final Logger log = LoggerFactory.getLogger(TimelinePane.class);
+
+
+    private static final PeriodFormatter FORMATTER = new PeriodFormatterBuilder().
+            appendMinutes().appendSuffix("m").appendSeconds().appendSuffix("s").toFormatter();
 
     private DoubleProperty msProperty = new SimpleDoubleProperty(this, "msProperty");
     private DoubleProperty secondWidthProperty = new SimpleDoubleProperty(this, "secondWidthProperty");
@@ -43,19 +50,17 @@ public class TimelinePane extends Pane {
     public void refresh() {
         getChildren().clear();
 
-        final int interval = 5;
+        final long everySeconds = 5 * 1000;
 
-        for (int second = 0; second < msProperty.doubleValue() / 1000; second += interval) {
-            if (second % (60 / interval) == 0) {
-                Text secondLabel = new Text(String.valueOf(second));
-                secondLabel.xProperty().bind(secondWidthProperty.multiply(second));
-                secondLabel.yProperty().bind(heightProperty().subtract(5));
-                secondLabel.setTranslateX(-(secondLabel.getBoundsInParent().getWidth() / 2));
-                secondLabel.getStyleClass().add("secondLabel");
-                secondLabel.setFontSmoothingType(FontSmoothingType.LCD);
-                getChildren().add(secondLabel);
-                log.trace("Added text label {}", secondLabel);
-            }
+        for (long ms = 0; ms < msProperty.doubleValue(); ms += everySeconds) {
+            Text secondLabel = new Text(FORMATTER.print(Duration.millis(ms).toPeriod()));
+            secondLabel.xProperty().bind(secondWidthProperty.multiply(ms / 1000));
+            secondLabel.yProperty().bind(heightProperty().subtract(5));
+            secondLabel.setTranslateX(-(secondLabel.getBoundsInParent().getWidth() / 2));
+            secondLabel.getStyleClass().add("secondLabel");
+            secondLabel.setFontSmoothingType(FontSmoothingType.LCD);
+            getChildren().add(secondLabel);
+            log.trace("Added text label {}", secondLabel);
         }
     }
 }
