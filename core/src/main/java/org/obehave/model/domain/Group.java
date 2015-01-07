8fd900c9acabdaa1c76da8cc96a51801ecda8600
Markus Möslinger <markus.moeslinger@gmail.com>
2015-01-08 00:19:33 +0100

    #59 Datenmodell
    - study now has groups

--- core/src/main/java/org/obehave/model/domain/Group.java
+++ core/src/main/java/org/obehave/model/domain/Group.java
@@ -8,4 +8,34 @@ import java.util.List;
 public class Group<T> {
     private List<Group<T>> subGroups;
     private List<T> elements;
+
+    public boolean contains(T element) {
+        if (elements.contains(element)) {
+            return true;
+        }
+
+        for(Group<T> group : subGroups) {
+            if (group.contains(element)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean add(T element) {
+        return elements.add(element);
+    }
+
+    public boolean add(Group<T> subGroup) {
+        return subGroups.add(subGroup);
+    }
+
+    public boolean remove(T element) {
+        return elements.remove(element);
+    }
+
+    public boolean remove(Group<T> subGroup) {
+        return subGroups.remove(subGroup);
+    }
 }
