5ce4eb158ef939553427e48501dbfcd7396e5f85
Patrick Erber <patrick.erber@gmail.com>
2015-02-03 17:27:40 +0100

    cleaned db helper

--- mobile/src/main/java/org/obehave/android/database/MyDatabaseHelper.java
+++ mobile/src/main/java/org/obehave/android/database/MyDatabaseHelper.java
@@ -28,8 +28,8 @@ public class MyDatabaseHelper extends OrmLiteSqliteOpenHelper{
     //private Dao<ActionGroup, Long> actionGroupDao;
 
 
-    public MyDatabaseHelper(Context context){
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    public MyDatabaseHelper(Context context, String dbFilename){
+        super(context, dbFilename, null, DATABASE_VERSION);
         Log.d(LOG_TAG, "database helper created");
     }
 
@@ -47,12 +47,9 @@ public class MyDatabaseHelper extends OrmLiteSqliteOpenHelper{
 
     private void createTables(SQLiteDatabase db, ConnectionSource connectionSource1) throws SQLException {
         Log.i(LOG_TAG, "create tables");
-        TableUtils.createTable(connectionSource, Study.class);
         TableUtils.createTable(connectionSource, Subject.class);
         TableUtils.createTable(connectionSource, Action.class);
         TableUtils.createTable(connectionSource, Observation.class);
-        //TableUtils.createTable(connectionSource, SubjectGroup.class);
-        //TableUtils.createTable(connectionSource, ActionGroup.class);;
     }
 
 
@@ -70,12 +67,9 @@ public class MyDatabaseHelper extends OrmLiteSqliteOpenHelper{
 
     private void dropTables(SQLiteDatabase db, ConnectionSource connectionSource) throws SQLException {
         boolean ignoreErrors = true;
-        TableUtils.dropTable(connectionSource, Study.class, ignoreErrors);
         TableUtils.dropTable(connectionSource, Subject.class, ignoreErrors);
         TableUtils.dropTable(connectionSource, Observation.class, ignoreErrors);
         TableUtils.dropTable(connectionSource, Action.class, ignoreErrors);
-        //TableUtils.dropTable(connectionSource, SubjectGroup.class, ignoreErrors);
-        //TableUtils.dropTable(connectionSource, ActionGroup.class, ignoreErrors);
     }
 
     public Dao<Study, Long> getStudyDao() throws SQLException {
@@ -99,79 +93,26 @@ public class MyDatabaseHelper extends OrmLiteSqliteOpenHelper{
         }
         return actionDao;
     }
-/*
-    public Dao<ActionGroup, Long> getActionGroupDao() throws SQLException{
-        if(actionGroupDao == null){
-            actionGroupDao = getDao(ActionGroup.class);
-        }
-        return actionGroupDao;
-    }
-    */
 
-    public Dao<Action, Long> getObservationDao() throws SQLException{
-        if(observationDao == null){
-            observationDao = getDao(Observation.class);
-        }
-        return actionDao;
-    }
-/*
-    public Dao<SubjectGroup, Long> getSubjectGroupDao() throws SQLException{
-        if(subjectGroupDao == null){
-            subjectGroupDao = getDao(SubjectGroup.class);
-        }
-        return subjectGroupDao;
-    }
-    */
 
-    private void createTestEntries() throws SQLException {
-            Study study = new Study();
-            study.setName("Test Study");
-          //  generateTestEntries(study);
-            getStudyDao().create(study);
+
+    private void createTestEntries() throws SQLException {;
+            generateTestEntries();
     }
-/*
-    private void generateTestEntries(Study study) throws SQLException {
+
+
+
+    private void generateTestEntries() throws SQLException {
         Subject subject;
-        study.setName("Android TestStudy");
-        SubjectGroup sg1 = new SubjectGroup("Gruppe 1");
-        SubjectGroup sg2 = new SubjectGroup("Gruppe 2");
-        Dao<SubjectGroup, Long> subjectGroupDao = getSubjectGroupDao();
-        subjectGroupDao.create(sg1);
-        subjectGroupDao.create(sg2);
-
-        Dao<ActionGroup, Long> actionGroupDao = getActionGroupDao();
-        ActionGroup ag1 = new ActionGroup("ActionGroup 1");
-        ActionGroup ag2 = new ActionGroup("ActionGroup 2");
-        actionGroupDao.create(ag1);
-        actionGroupDao.create(ag2);
 
         for(int i = 0; i < 30; i++) {
-
             subject = new Subject("Subject " + i);
             Action action = new Action("Action " + i);
-            if(i < 15){
-                subject.setSubjectGroup(sg1);
-                action.setActionGroup(ag1);
-            }
-            else {
-                subject.setSubjectGroup(sg2);
-                action.setActionGroup(ag2);
-            }
             Dao<Subject, Long> subjectDao = getSubjectDao();
             Dao<Action, Long> actionDao = getActionDao();
             subjectDao.create(subject);
             actionDao.create(action);
-            study.addSubject(subject);
-            study.addAction(action);
         }
-        Dao<Subject, Long> subjectDao = getSubjectDao();
-        subject = new Subject("Subject 40");
-        subject.setSubjectGroup(null);
-        subjectDao.create(subject);
-        subject = new Subject("Subject 41");
-        subject.setSubjectGroup(null);
-        subjectDao.create(subject);
     }
 
-*/
 }
