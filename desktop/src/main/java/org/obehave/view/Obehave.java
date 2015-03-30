f532ace94306e82caf3bb5f1264e2d12fe433047
Markus Möslinger <markus.moeslinger@gmail.com>
2015-03-30 11:22:00 +0200

    - refactored package structure... quite a bit.

--- desktop/src/main/java/org/obehave/view/Obehave.java
+++ desktop/src/main/java/org/obehave/view/Obehave.java
@@ -8,7 +8,7 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import org.obehave.events.EventBusHolder;
 import org.obehave.util.I18n;
-import org.obehave.view.controller.MainController;
+import org.obehave.view.components.MainController;
 
 import java.net.URL;
 
@@ -17,7 +17,7 @@ public class Obehave extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        final URL mainFxml = getClass().getClassLoader().getResource("ui/main.fxml");
+        final URL mainFxml = getClass().getClassLoader().getResource("org/obehave/view/components/main.fxml");
         if (mainFxml == null) {
             throw new IllegalStateException("Couldn't load main.fxml!");
         }
@@ -35,12 +35,12 @@ public class Obehave extends Application {
         stage.setScene(scene);
         stage.setMaximized(true);
 
-        stage.getIcons().addAll(new Image(getClass().getClassLoader().getResourceAsStream("icons/icon_16x16.png")),
-                new Image(getClass().getClassLoader().getResourceAsStream("icons/icon_32x32.png")),
-                new Image(getClass().getClassLoader().getResourceAsStream("icons/icon_48x48.png")),
-                new Image(getClass().getClassLoader().getResourceAsStream("icons/icon_96x96.png")),
-                new Image(getClass().getClassLoader().getResourceAsStream("icons/icon_144x144.png")),
-                new Image(getClass().getClassLoader().getResourceAsStream("icons/icon_256x256.png")));
+        stage.getIcons().addAll(new Image(getClass().getClassLoader().getResourceAsStream("org/obehave/view/icons/icon_16x16.png")),
+                new Image(getClass().getClassLoader().getResourceAsStream("org/obehave/view/icons/icon_32x32.png")),
+                new Image(getClass().getClassLoader().getResourceAsStream("org/obehave/view/icons/icon_48x48.png")),
+                new Image(getClass().getClassLoader().getResourceAsStream("org/obehave/view/icons/icon_96x96.png")),
+                new Image(getClass().getClassLoader().getResourceAsStream("org/obehave/view/icons/icon_144x144.png")),
+                new Image(getClass().getClassLoader().getResourceAsStream("org/obehave/view/icons/icon_256x256.png")));
 
         EventBusHolder.register(mainController);
         mainController.chooseStudy();
---
+++ desktop/src/main/java/org/obehave/view/components/tree/edit/ObservationEditControl.java
@@ -0,0 +1,145 @@
+package org.obehave.view.components.tree.edit;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import org.joda.time.DateTime;
+import org.obehave.model.Node;
+import org.obehave.model.Observation;
+import org.obehave.service.NodeService;
+import org.obehave.service.ObservationService;
+import org.obehave.view.Obehave;
+import org.obehave.view.util.AlertUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.time.LocalDate;
+
+/**
+ * @author Markus Möslinger
+ */
+public class ObservationEditControl {
+    private static final Logger log = LoggerFactory.getLogger(ObservationEditControl.class);
+    private static final ObservationService observationService = ObservationService.getInstance();
+    private static final NodeService nodeService = NodeService.getInstance();
+
+    private Node loadedObservationNode;
+
+    private Runnable saveCallback;
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private Button video;
+
+    private File videoPath;
+
+    @FXML
+    private DatePicker date;
+
+    @FXML
+    private TextField hour;
+    @FXML
+    private TextField minute;
+
+    public String getName() {
+        return name.getText();
+    }
+
+    public void setName(String name) {
+        this.name.setText(name);
+    }
+
+    public void selectVideo() {
+        FileChooser chooser = new FileChooser();
+        chooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Video files", "*.mp4", "*.m4a", "*.m4v",
+                "*.m3u8", "*.fxm", "*.flv"));
+        setVideoPath(chooser.showOpenDialog(Obehave.STAGE));
+    }
+
+    public void setVideoPath(File path) {
+        videoPath = path;
+
+        if (videoPath != null) {
+            video.setText(videoPath.getName());
+        } else {
+            video.setText("Select video...");
+        }
+    }
+
+    public void loadObservation(Node node) {
+        loadedObservationNode = node;
+        Observation o = (Observation) node.getData();
+
+        if (o == null) {
+            setName("");
+            setVideoPath(null);
+        } else {
+            setName(o.getName());
+            setVideoPath(o.getVideo());
+        }
+
+        if (o != null && o.getDateTime() != null) {
+            DateTime dt = o.getDateTime();
+            date.setValue(LocalDate.of(dt.getYear(), dt.getMonthOfYear(), dt.getDayOfMonth()));
+            hour.setText(String.format("%02d", dt.getHourOfDay()));
+            minute.setText(String.format("%02d", dt.getMinuteOfHour()));
+        } else {
+            date.setValue(null);
+            hour.setText("00");
+            minute.setText("00");
+        }
+
+        name.requestFocus();
+    }
+
+    public void saveCurrent() {
+        if (name.getText().isEmpty()) {
+            AlertUtil.showError("Validation error", "Observation must have a name");
+            return;
+        }
+
+        Observation o;
+
+        if (loadedObservationNode.getData() == null) {
+            log.debug("Creating new observation");
+            o = new Observation();
+        } else {
+            log.debug("Saving existing observation");
+            o = (Observation) loadedObservationNode.getData();
+        }
+
+        o.setName(getName());
+
+        o.setVideo(videoPath);
+        LocalDate pickedDate = date.getValue();
+
+        if (pickedDate != null) {
+            DateTime dt = new DateTime(pickedDate.getYear(), pickedDate.getMonthValue(), pickedDate.getDayOfMonth(),
+                    Integer.valueOf(hour.getText()), Integer.valueOf(minute.getText()));
+
+            o.setDateTime(dt);
+        }
+
+        observationService.save(o);
+        if (loadedObservationNode.getData() == null) {
+            loadedObservationNode.addChild(o);
+        }
+        nodeService.save(loadedObservationNode);
+
+        loadedObservationNode = null;
+        saveCallback.run();
+    }
+
+    public void cancel() {
+        saveCallback.run();
+    }
+
+    public void setSaveCallback(Runnable saveCallback) {
+        this.saveCallback = saveCallback;
+    }
+}
