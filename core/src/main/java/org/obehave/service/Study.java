77434b49f7f05ee40fa83055ce442f24231b7c4a
Markus Möslinger <markus.moeslinger@gmail.com>
2015-03-31 02:52:55 +0200

    - removed testing methods from study

--- core/src/main/java/org/obehave/service/Study.java
+++ core/src/main/java/org/obehave/service/Study.java
@@ -1,8 +1,6 @@
 package org.obehave.service;
 
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
-import org.obehave.events.ChangeEvent;
-import org.obehave.events.ChangeType;
 import org.obehave.events.EventBusHolder;
 import org.obehave.events.RepaintStudyEvent;
 import org.obehave.exceptions.Validate;
@@ -127,47 +125,8 @@ public class Study implements Displayable {
         return subjects;
     }
 
-    public boolean addSubject(Subject subject) {
-        if (subjects.contains(subject)) {
-            log.debug("Won't setData another {}", subject);
-            return false;
-        }
-
-        log.debug("Adding subject {}", subject);
-
-        subjects.addChild(subject);
-        EventBusHolder.post(new ChangeEvent<>(subject, ChangeType.CREATE));
-        return true;
-    }
-
-    public boolean removeSubject(Subject subject) {
-        log.debug("Removing subject {}", subject);
-        final boolean deleted = subjects.remove(subject);
-        EventBusHolder.post(new ChangeEvent<>(subject, ChangeType.DELETE));
-        return deleted;
-    }
-
     public Node getActions() {
         return actions;
-    }
-
-    public boolean addAction(Action action) {
-        if (actions.contains(action)) {
-            log.debug("Won't setData another {}", action);
-            return false;
-        }
-
-        log.debug("Adding action {}", action);
-        actions.addChild(action);
-        EventBusHolder.post(new ChangeEvent<>(action, ChangeType.CREATE));
-        return true;
-    }
-
-    public boolean removeAction(Action action) {
-        log.debug("Removing action {}", action);
-        final boolean deleted = actions.remove(action);
-        EventBusHolder.post(new ChangeEvent<>(action, ChangeType.DELETE));
-        return deleted;
     }
 
     public Node getObservations() {
@@ -176,37 +135,6 @@ public class Study implements Displayable {
 
     public Node getModifierFactories() {
         return modifierFactories;
-    }
-
-    public boolean addObservation(Observation observation) {
-        if (observations.contains(observation)) {
-            log.debug("Won't setData another {}", observation);
-            return false;
-        }
-
-        log.debug("Adding observation {}", observation);
-        observations.addChild(observation);
-        EventBusHolder.post(new ChangeEvent<>(observation, ChangeType.CREATE));
-        return true;
-    }
-
-    public boolean addModifierFactory(ModifierFactory modifierFactory) {
-        if (modifierFactories.contains(modifierFactory)) {
-            log.debug("Won't setData another {}", modifierFactory);
-            return false;
-        }
-
-        log.debug("Adding modifierFactory {}", modifierFactory);
-        modifierFactories.addChild(modifierFactory);
-        EventBusHolder.post(new ChangeEvent<>(modifierFactory, ChangeType.CREATE));
-        return true;
-    }
-
-    public boolean removeObservation(Observation observation) {
-        log.debug("Removing observation {}", observation);
-        final boolean deleted = observations.remove(observation);
-        EventBusHolder.post(new ChangeEvent<>(observation, ChangeType.DELETE));
-        return deleted;
     }
 
     public String getName() {
@@ -219,24 +147,6 @@ public class Study implements Displayable {
         DatabaseProperties.set(DatabaseProperties.STUDY_NAME, name);
 
         EventBusHolder.post(new RepaintStudyEvent());
-    }
-
-    // ONLY FOR TEMPORARILY TESTING!
-    public void addRandomSubject(String key) {
-        addSubject(new Subject(getRandomString("Wolf " + key)));
-    }
-
-    public void addRandomAction(String key) {
-        addAction(new Action(getRandomString("Action " + key)));
-    }
-
-    public void addRandomObservation(String key) {
-        addObservation(new Observation(getRandomString("Observation " + key)));
-    }
-
-    private static String getRandomString(String prefix) {
-        int number = (int) (Math.random() * 5);
-        return prefix + " " + number;
     }
 
     public File getSavePath() {
