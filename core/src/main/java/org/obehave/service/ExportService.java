1be189dc7ba0825e9f2b3a5ea5f679f88233f43e
Stefan Lamprecht <mail@x3n.at>
2015-04-01 17:02:11 +0200

    exportservicetest and exportservice modified

--- core/src/main/java/org/obehave/service/ExportService.java
+++ core/src/main/java/org/obehave/service/ExportService.java
@@ -11,6 +11,8 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 /**
@@ -30,16 +32,15 @@ public class ExportService {
 
     public void exportAction(List<Observation> observations, List<Subject> subjects, Action action) {
 
-        //keep index of subjects in sync with maps
-        final List<Subject> finalSubjects = subjects;
 
         // Map contains counts per actions by subject, LinkedHashMap to preserve order
-        Map<String,Integer[]> countsPerAction = new LinkedHashMap<String, Integer[]>();
-        Map<String,Long[]> totalDurationPerAction = new LinkedHashMap<String, Long[]>();
+        Map<String, ArrayList<Long>> countsPerAction = new LinkedHashMap<String, ArrayList<Long>>();
+        Map<String, ArrayList<Long>> totalDurationPerAction = new LinkedHashMap<String, ArrayList<Long>>();
 
         // Initialize map key and values
-        for (Subject s : finalSubjects){
-            countsPerAction.put(s.getName(), new Integer[finalSubjects.size()]);
+        for (Subject s : subjects) {
+            countsPerAction.put(s.getName(), new ArrayList<Long>(Collections.nCopies(subjects.size(), 0L)));
+            totalDurationPerAction.put(s.getName(), new ArrayList<Long>(Collections.nCopies(subjects.size(), 0L)));
         }
 
 
@@ -48,58 +49,63 @@ public class ExportService {
             for (Coding c : o.getCodings()) {
 
                 // is this any good really?
-                String subjectName = c.getSubject().getName();
-                Integer[] counts = countsPerAction.get(subjectName);
-                Long[] durations = totalDurationPerAction.get(subjectName);
-                Long duration = c.getDuration();
+                Subject subject = c.getSubject();
+
 
                 //stirring for right stuff
-                if (isRequestedSubjectAndAction(finalSubjects, action, c, subjectName) && isRequestedSubjectModifier(c, finalSubjects)) {
-                    counts[finalSubjects.indexOf(c.getModifier().get())] += 1;
-                    durations[finalSubjects.indexOf(c.getModifier().get())] += duration;
-                } else if (isRequestedSubjectAndAction(finalSubjects, action, c, subjectName)  && c.getModifier()==null) {
-                    counts[finalSubjects.indexOf(subjectName)] += 1;
-                    durations[finalSubjects.indexOf(subjectName)] += duration;
+                if (isRequestedSubjectAndAction(subjects, action, c, subject) && c.getModifier() == null) {
+                    Long value = countsPerAction.get(subject.getName()).get(subjects.indexOf(subject));
+                    countsPerAction.get(subject.getName()).set(subjects.indexOf(subject),value+1);
+                    Long duration = totalDurationPerAction.get(subject.getName()).get(subjects.indexOf(subject));
+                    totalDurationPerAction.get(subject.getName()).set(subjects.indexOf(subject),duration+c.getDuration());
+
+                } else if (isRequestedSubjectAndAction(subjects, action, c, subject) && isRequestedSubjectModifier(c, subjects)) {
+                    long value = countsPerAction.get(subject.getName()).get(subjects.indexOf(c.getModifier().get()));
+                    countsPerAction.get(subject.getName()).set(subjects.indexOf(c.getModifier().get()),value+1);
+                    Long duration = totalDurationPerAction.get(subject.getName()).get(subjects.indexOf(c.getModifier().get()));
+                    totalDurationPerAction.get(subject.getName()).set(subjects.indexOf(c.getModifier().get()),duration+c.getDuration());
                 }
             }
         }
+
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        generateExcelSheet(workbook, countsPerAction, action, subjects, "totalCounts");
+        generateExcelSheet(workbook, totalDurationPerAction, action, subjects, "totalDuration");
+        writeExcelSheetToDisk(workbook, action);
+
     }
 
-    private boolean isRequestedSubjectAndAction(List<Subject> subjects, Action action, Coding c, String subjectName) {
-        return subjects.contains(subjectName) && c.getAction().equals(action);
+    private boolean isRequestedSubjectAndAction(List<Subject> subjects, Action action, Coding c, Subject subject) {
+        return subjects.contains(subject) && c.getAction().equals(action);
     }
 
     private boolean isRequestedSubjectModifier(Coding c, List<Subject> subjects) {
         return subjects.contains(((c.getModifier().getType() == Modifier.Type.SUBJECT_MODIFIER) && subjects.contains(c.getModifier().get())));
     }
 
-    private XSSFSheet generateExcelSheet(Map<String, Integer[]> countsPerAction, Action action, List<Subject> finalSubjects){
-
-        //Blank workbook
-        XSSFWorkbook workbook = new XSSFWorkbook();
+    private XSSFSheet generateExcelSheet(XSSFWorkbook wb, Map<String, ArrayList<Long>> data, Action action, List<Subject> finalSubjects, String type) {
 
         //Create a blank sheet and set name like the action
-        XSSFSheet sheet = workbook.createSheet(action.getName());
+        XSSFSheet sheet = wb.createSheet(action.getName() + "-" + type);
 
         //Iterate over data and write to sheet
-        Set<String> keyset = countsPerAction.keySet();
+        Set<String> keyset = data.keySet();
 
         int rownum = 0;
 
         //set matrix title from left to right
         //TODO Set cell font color like subject color
-        Row row =sheet.createRow(rownum++);
-        for (Subject subject : finalSubjects){
+        Row row = sheet.createRow(rownum++);
+        for (Subject subject : finalSubjects) {
             int cellNum = 1;
             Cell cell = row.createCell(cellNum++);
             cell.setCellValue(subject.getName());
         }
 
         //set matrix title from top to bottom and set values
-        for (String key : keyset)
-        {
+        for (String key : keyset) {
             sheet.createRow(rownum++);
-            Integer[] intArr = countsPerAction.get(key);
+            ArrayList<Long> longArray = data.get(key);
             int cellnum = 0;
 
             //title
@@ -107,32 +113,29 @@ public class ExportService {
             title.setCellValue(key);
 
             //values
-            for (Integer values : intArr)
-            {
+            for (Long values : longArray) {
                 Cell cell = row.createCell(cellnum++);
                 cell.setCellValue(values);
-//                if(values instanceof String)
-//                    cell.setCellValue((String)values);
-//                else if(values instanceof Integer)
-//                    cell.setCellValue((Integer)values);
             }
         }
         return null;
     }
 
-    public void writeExcelSheetToDisk(XSSFWorkbook workbook, File savePath){
-        try
-        {
+    public void writeExcelSheetToDisk(XSSFWorkbook workbook, Action action) {
+
+        String fileSeparator = System.getProperty("file.separator");
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        String blank = " ";
+
+        try {
             log.debug("Start writing Excel to {}", savePath.getAbsolutePath());
             //Write the workbook to the file system
-            FileOutputStream out = new FileOutputStream(savePath);
+            FileOutputStream out = new FileOutputStream(savePath + fileSeparator + "Export" + blank + dateFormat.format(new Date()) + blank + action.getName());
             workbook.write(out);
             out.close();
             log.debug("Excel successfully written to {}", savePath.getAbsolutePath());
-        }
-        catch (Exception e)
-        {
-            log.debug("Something has gone terribly wrong: {}", e.getMessage(),e);
+        } catch (Exception e) {
+            log.debug("Something has gone terribly wrong: {}", e.getMessage(), e);
             e.printStackTrace();
         }
     }
