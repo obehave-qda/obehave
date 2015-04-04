dda364bb89268690cb8dd40e39f165daea18cd9a
Markus Möslinger <markus.moeslinger@gmail.com>
2015-04-04 20:01:55 +0200

    - created CodingRange and tests for it - fixed some bugs in coding

--- core/src/test/java/org/obehave/model/CodingTest.java
+++ core/src/test/java/org/obehave/model/CodingTest.java
@@ -3,9 +3,6 @@ package org.obehave.model;
 import org.junit.Before;
 import org.junit.Test;
 import org.obehave.exceptions.FactoryException;
-import org.obehave.model.Action;
-import org.obehave.model.Coding;
-import org.obehave.model.Subject;
 import org.obehave.model.modifier.ModifierFactory;
 
 import java.math.BigDecimal;
@@ -17,27 +14,23 @@ import static org.junit.Assert.assertEquals;
  */
 public class CodingTest {
     private final Subject subject = new Subject("Test subject");
-    private final Action action = new Action("Test action");
+    private final Action stateAction = new Action("Test state action", Action.Type.STATE);
     private final long millis = 500;
     private final long endMillis = millis + 250;
 
-    private Coding coding;
     private Coding stateCoding;
 
     @Before
     public void prepare() throws FactoryException {
-        coding = new Coding(subject, action, millis);
-        stateCoding = new Coding(subject, action, millis, endMillis);
+        stateCoding = new Coding(subject, stateAction, millis, endMillis);
 
-        action.setModifierFactory(new ModifierFactory(0, 10));
-
-        coding.setModifier("3");
+        stateAction.setModifierFactory(new ModifierFactory(0, 10));
         stateCoding.setModifier("3");
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void noCreationWithoutSubject() {
-        new Coding(null, action, millis);
+        new Coding(null, stateAction, millis);
     }
 
     @Test(expected = IllegalArgumentException.class)
@@ -45,15 +38,10 @@ public class CodingTest {
         new Coding(subject, null, millis);
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void noCreationWithNullAsModifierInput() throws FactoryException {
-        new Coding(subject, action, null, millis);
-    }
-
     @Test
     public void accessingFieldsShouldWork() throws FactoryException {
         assertEquals(subject, stateCoding.getSubject());
-        assertEquals(action, stateCoding.getAction());
+        assertEquals(stateAction, stateCoding.getAction());
         assertEquals(millis, stateCoding.getStartMs());
         assertEquals(BigDecimal.valueOf(3), stateCoding.getModifier().get());
         assertEquals(endMillis - millis, stateCoding.getDuration());
@@ -61,16 +49,11 @@ public class CodingTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void noCreationWithoutSubjectButMillis() {
-        new Coding(null, action, millis, endMillis);
+        new Coding(null, stateAction, millis, endMillis);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void noCreationWithoutActionButMillis() {
         new Coding(subject, null, millis, endMillis);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void noCreationWithNullAsModifierInputButMillis() throws FactoryException {
-        new Coding(subject, action, null, millis, endMillis);
     }
 }
